acad774dec499748dcdefecf3ace99a6202f3d48
arctavares <cosilk@hotmail.com>
2024-04-30 16:51:34 -0300

    add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Carousel from './components/Carousel';
 import Header from './components/Header';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header />
+      <Carousel />
     </>
   )
 }
