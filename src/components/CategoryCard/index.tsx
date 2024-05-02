e0d5c8794abfe1aad61fe819794d655a863acb3f
arctavares <cosilk@hotmail.com>
2024-05-02 02:49:29 -0300

    add category style

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -3,8 +3,8 @@ export default function CategoryCard() {
         <div className="col-6 mx-auto g-4 w-100 h-100 d-flex align-items-stretch">
         <div className="col-6 mx-auto g-4 w-100 h-100 mb-4">
             <div className="card h-100">
-            <img src="https://m.media-amazon.com/images/I/619QaGrReCL._AC_UF894,1000_QL80_.jpg" alt="landscape" className="bd-placeholder-img card-img-top"/>
-            <div className="card-header">
+            <img src="https://img.freepik.com/fotos-gratis/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg" alt="landscape" className="bd-placeholder-img card-img-top"/>
+            <div className="card-header bg-black text-white">
                 <p className="text-center">Categoria</p>
             </div>
             </div>
