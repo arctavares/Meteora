5d969ad7d8a93eec6919b5cc027198b7a7d4804c
arctavares <cosilk@hotmail.com>
2024-05-02 03:12:53 -0300

    add style to subscribe

--- src/components/Subscribe/index.tsx
+++ src/components/Subscribe/index.tsx
@@ -4,7 +4,7 @@ export default function Subscribe() {
             <h5>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</h5>
         <div className="input-group my-3">
             <input className="form-control border-secondary rounded-0" type="email" placeholder="Digite seu e-mail" aria-label="Digite seu e-mail" aria-describedby="button-addon2" /> 
-            <button type="button" id="button-addon2">Enviar</button>
+            <button type="button" id="button-addon2" className="sendBtn">Enviar</button>
         </div>
       </form>
     )
