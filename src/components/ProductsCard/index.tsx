ffe5f5abb040466000528ab386de44ca9ff9d1a2
arctavares <cosilk@hotmail.com>
2024-05-02 03:00:37 -0300

    add productsCard style

--- src/components/ProductsCard/index.tsx
+++ src/components/ProductsCard/index.tsx
@@ -4,10 +4,10 @@ export default function ProductsCard () {
         <div className="card">
           <img src="https://png.pngtree.com/background/20230618/original/pngtree-lakescape-landscape-nature-scenery-hd-picture-image_3712644.jpg" alt="landscape" className="bd-placeholder-img card-img-top"/>
           <div className="card-body">
-            <h5 className="card-title">Produto</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <p>R$ 30,00</p>
-            <a href="#" className="btn btn-primary">Ver mais</a>
+            <h5 className="card-title fs-4 fw-bold">Produto</h5>
+            <p className="card-text fs-6 mb-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="fs-5 mb-3 fw-bold">R$ 30,00</p>
+            <a href="#" className="btn btn-primary moreBtn rounded-0 fw-bold">Ver mais</a>
           </div>
         </div>
     </div>
