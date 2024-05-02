cf14d86acae1583369bbf4bc3730b06851f4d6cb
arctavares <cosilk@hotmail.com>
2024-05-02 17:40:08 -0300

    add categories images

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -2,15 +2,59 @@ import CategoryCard from "../CategoryCard"
 
 export default function Categories () {
     
+    const categories = [
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-bolsa.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-bolsas.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-bolsas.png',
+            text: 'Bolsas'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-calcados.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-calcados.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-calcados.png',
+            text: 'Calçados'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-calcas.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-calcas.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-calcas.png',
+            text: 'Calças'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-camiseta.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-camiseta.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-camiseta.png',
+            text: 'Camisetas'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-casacos.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-casacos.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-casacos.png',
+            text: 'Casacos'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/categorias/categoria-oculos.png',
+            imgTablet: 'src/assets/Tablet/categorias/categoria-oculos.png',
+            imgMobile: 'src/assets/Tablet/categorias/categoria-oculos.png',
+            text: 'Óculos'
+        }
+    ];
+    
+
     function renderCards() {
-        return [...Array(6)].map((_, index) => {
-            return <div key={index} className="col-6 col-md-4 col-xxl-2 h-100"><CategoryCard /></div>;
+        return categories.map((category, index) => {
+            return (
+                <div key={index} className="col-6 col-md-4 col-xxl-2 h-100">
+                    <CategoryCard category={category} />
+                </div>
+            );
         });
     }
     
     return (
         <div className="container">
-        <h2 className="text-center p-4">Busque por categoria:</h2>
+        <h2 className="text-center p-4 fs-3">Busque por categoria:</h2>
         <div className="row mx-auto">
                 {renderCards()}
         </div>
--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -1,11 +1,22 @@
-export default function CategoryCard() {
+interface CategoryCardProps {
+    category: {
+        imgDesktop: string,
+        imgTablet: string,
+        imgMobile: string,
+        text: string,
+    }
+}
+
+export default function CategoryCard(props: CategoryCardProps) {
     return (
         <div className="col-6 mx-auto g-4 w-100 h-100 d-flex align-items-stretch">
         <div className="col-6 mx-auto g-4 w-100 h-100 mb-4">
             <div className="card h-100">
-            <img src="https://img.freepik.com/fotos-gratis/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg" alt="landscape" className="bd-placeholder-img card-img-top"/>
+            <img src={props.category.imgDesktop} alt="landscape" className="bd-placeholder-img card-img-top d-none d-xl-block"/>
+            <img src={props.category.imgTablet} alt="landscape" className="bd-placeholder-img card-img-top d-none d-lg-block d-xl-none"/>
+            <img src={props.category.imgMobile} alt="landscape" className="bd-placeholder-img card-img-top d-lg-none"/>
             <div className="card-header bg-black text-white">
-                <p className="text-center">Categoria</p>
+                <p className="text-center">{props.category.text}</p>
             </div>
             </div>
         </div>
