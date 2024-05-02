8a6dfc323e43950cad4f8ccde3348a826a98c747
arctavares <cosilk@hotmail.com>
2024-05-02 03:14:17 -0300

    add style to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="text-center">
+        <footer className="text-center bg-black text-white">
             <p className="py-3">2023 <i className="bi bi-c-circle"></i> Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
         </footer>
     )
