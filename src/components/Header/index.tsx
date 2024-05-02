8cf2003e6a18f0423e1d196fb410fd582c2f0593
arctavares <cosilk@hotmail.com>
2024-05-02 02:43:02 -0300

    add style to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 export default function Header () {
     return (
-        <nav className="navbar navbar-expand-md bg-body-tertiary">
+        <nav className="navbar navbar-expand-md bg-black navbar-dark">
         <div className="container-fluid ">
-            <a className="navbar-brand" href="#">Meteora</a>
+            <a className="navbar-brand" href="#"><h1 className="m-0"><img className="d-block" src="src/assets/logo-meteora.png" alt="meteora" /></h1></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -22,8 +22,8 @@ export default function Header () {
                 </li>
             </ul>
         <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Digite o produto" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Buscar</button>
+            <input className="form-control me-2 rounded-0" type="search" placeholder="Digite o produto" aria-label="Search" />
+            <button className="btn btn-outline-light rounded-0" type="submit">Buscar</button>
         </form>
     </div>
   </div>
