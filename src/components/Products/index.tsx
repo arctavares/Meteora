e3ae527cdf4d266e49cf6619d88cdaea7084ce47
arctavares <cosilk@hotmail.com>
2024-05-03 16:25:47 -0300

    add images to products

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -2,15 +2,67 @@ import ProductsCard from "../ProductsCard"
 
 export default function Products () {
 
+    const cards = [
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/bolsa.png',
+            imgTablet: 'src/assets/Tablet/produtos/bolsa.png',
+            imgMobile: 'src/assets/Mobile/produtos/bolsa.png',
+            title: 'Bolsa coringa',
+            text: 'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!',
+            price: 'R$120,00'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/calca.png',
+            imgTablet: 'src/assets/Tablet/produtos/calca.png',
+            imgMobile: 'src/assets/Mobile/produtos/calca.png',
+            title: 'Calça jeans básica',
+            text: 'Calça jeans azul básica, confortável e versátil. Essencial para o seu guarda-roupa!',
+            price: 'R$80,00'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/camiseta.png',
+            imgTablet: 'src/assets/Tablet/produtos/camiseta.png',
+            imgMobile: 'src/assets/Mobile/produtos/camiseta.png',
+            title: 'Camiseta casual',
+            text: 'Camiseta branca básica, ótima para o dia a dia. Combina com qualquer estilo!',
+            price: 'R$40,00'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/jaqueta-jeans.png',
+            imgTablet: 'src/assets/Tablet/produtos/jaqueta-jeans.png',
+            imgMobile: 'src/assets/Mobile/produtos/jaqueta-jeans.png',
+            title: 'Jaqueta jeans clássica',
+            text: 'Jaqueta jeans azul clássica, resistente e estilosa. Perfeita para todas as estações!',
+            price: 'R$150,00'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/oculos.png',
+            imgTablet: 'src/assets/Tablet/produtos/oculos.png',
+            imgMobile: 'src/assets/Mobile/produtos/oculos.png',
+            title: 'Óculos de sol',
+            text: 'Óculos de sol preto, armação leve e lentes polarizadas. Proteção e estilo garantidos!',
+            price: 'R$90,00'
+        },
+        {
+            imgDesktop: 'src/assets/Desktop/produtos/tenis.png',
+            imgTablet: 'src/assets/Tablet/produtos/tenis.png',
+            imgMobile: 'src/assets/Mobile/produtos/tenis.png',
+            title: 'Tênis esportivo',
+            text: 'Tênis esportivo cinza, confortável e moderno. Ideal para praticar atividades físicas!',
+            price: 'R$200,00'
+        }
+    ];
+    
+
     function renderCards () {
-        return [...Array(6)].map(() => {
-            return (<ProductsCard />)
+        return cards.map((card) => {
+            return (<ProductsCard card={card}/>)
         })
     }
 
     return (
         <div className="container">
-        <h2 className="text-center p-4">Produtos que estão bombando!</h2>
+        <h2 className="text-center p-4 fs-3">Produtos que estão bombando!</h2>
         <div className="container row mx-auto">
             {renderCards()}
         </div>
--- src/components/ProductsCard/index.tsx
+++ src/components/ProductsCard/index.tsx
@@ -1,15 +1,33 @@
-export default function ProductsCard () {
+interface Card {
+  imgDesktop: string;
+  imgTablet: string;
+  imgMobile: string;
+  title: string;
+  text: string;
+  price: string;
+}
+
+
+interface ProductsCardProps {
+  card: Card;
+}
+
+const ProductsCard: React.FC<ProductsCardProps> = ({ card }) => {
     return (
         <div className="container col-lg-4 col-md-6 col-12 mb-3">
         <div className="card">
-          <img src="https://png.pngtree.com/background/20230618/original/pngtree-lakescape-landscape-nature-scenery-hd-picture-image_3712644.jpg" alt="landscape" className="bd-placeholder-img card-img-top"/>
+          <img src={card.imgDesktop} alt="landscape" className="bd-placeholder-img card-img-top d-none d-xl-block"/>
+          <img src={card.imgTablet} alt="landscape" className="bd-placeholder-img card-img-top d-none d-lg-block d-xl-none"/>
+          <img src={card.imgMobile} alt="landscape" className="bd-placeholder-img card-img-top d-lg-none"/>
           <div className="card-body">
-            <h5 className="card-title fs-4 fw-bold">Produto</h5>
-            <p className="card-text fs-6 mb-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <p className="fs-5 mb-3 fw-bold">R$ 30,00</p>
+            <h5 className="card-title fs-4 fw-bold">{card.title}</h5>
+            <p className="card-text fs-6 mb-3">{card.text}</p>
+            <p className="fs-5 mb-3 fw-bold">{card.price}</p>
             <a href="#" className="btn btn-primary moreBtn rounded-0 fw-bold">Ver mais</a>
           </div>
         </div>
     </div>
     )
 }
+
+export default ProductsCard;
